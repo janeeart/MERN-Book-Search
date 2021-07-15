105bf800707a30c5ff74fdce3fa3836d5a65ecb8
Janée Artis <janeeartis@gmail.com>
2021-07-14 22:51:31 -0600

    commit

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,72 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation loginUser( $email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+        user {
+            _id
+            username
+            email
+            bookCount
+            savedBooks{
+                authors
+                description 
+                bookId
+                image
+                link
+                title
+            }
+        }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: saveBook!) {
+    saveBook(input: $input) {
+       
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            authors
+            description 
+            bookId
+            image
+            link
+            title
+        }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $ID!) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            authors
+            description 
+            bookId
+            image
+            link
+            title
+        }
+    }
+  }
+`;
