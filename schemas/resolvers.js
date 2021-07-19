2c51bfcdcdc998e7c51783fa98097642d059832b
Janée Artis <janeeartis@gmail.com>
2021-07-18 18:34:56 -0600

    sign up not working

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -3,11 +3,10 @@ const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-
-  Query: {   
+  Query: {
     me: async (parent, args, context) => {
-      if (context.user) {
-        const userData = User.findOne({ _id: context.user._id }).select('-__v -password');
+      if (context,user) {
+        const userData = User.findOne({ _id: context.user.id }).select('-_v -password');
         return userData;
       }
       throw new AuthenticationError('You need to be logged in!');
@@ -16,28 +15,29 @@ const resolvers = {
 
   Mutation: {
     addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
+      const user = await User.create({ username, email, password});
       const token = signToken(user);
-      return { token, user };
+      return { token,user };
     },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
       if (!user) {
-        throw new AuthenticationError('No user found with this email address');
+        throw new AuthenticationError('No user with this email found!');
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError('Incorrect password!');
       }
 
       const token = signToken(user);
-
       return { token, user };
     },
+
     saveBook: async (parent, { bookData }, context) => {
+      // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
       if (context.user) {
         const updatedUser = await User.findByIdAndUpdate(
             { _id: context.user._id },
@@ -49,7 +49,6 @@ const resolvers = {
       }
       throw new AuthenticationError('You need to be logged in!');
     },
-  
     removeBook: async (parent, { bookId }, context) => {
       if (context.user) {
         const updatedUser = await User.findOneAndUpdate(
@@ -62,8 +61,36 @@ const resolvers = {
       }
       throw new AuthenticationError('You need to be logged in!');
     },
-  
   },
+
+    // saveBook: async (parent, { bookData }, context) => {
+    //   // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
+    //   if (context.user) {
+    //     const updatedUser = await User.findByIdAndUpdate(
+    //         { _id: context.user._id },
+    //         { $push: {savedBooks: { bookData } } },
+    //         { new: true }
+    //     );
+
+    //     return updatedUser;
+    //   }
+    //   throw new AuthenticationError('You need to be logged in!');
+    // },
+
+    // removeBook: async (parent, { bookId }, context) => {
+    //   if (context.user) {
+    //     const updatedUser = await User.findOneAndUpdate(
+    //       { _id: context.user._id },
+    //       { $pull: { savedBooks: { bookId} } },
+    //       { new: true }
+    //     );
+
+    //     return updatedUser;
+    //   }
+    //   throw new AuthenticationError('You need to be logged in!');
+    // },
+
+
 };
 
 module.exports = resolvers;
